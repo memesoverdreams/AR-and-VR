58afecd48f29997eb591d80c214adaae20194cc4
Gonk Droid <56324288+LinuxAmGood@users.noreply.github.com>
2020-01-12 16:06:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ $(function(){
     }
   });
 });
+
+function openNav() {
+  document.getElementById("myNav").style.width = "35%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
